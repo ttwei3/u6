8a1a1a4505a53d8246512b5f055ce879dec72bd1
Tingting Wei <twei3@academic.rrc.ca>
2024-02-29 14:25:59 -0600

    add action and view in controller, then define routes and motify the view template

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "customers#index"
   get 'customers/index'
+
+  get 'customers/missing_email', to: 'customers#missing_email'
+  get 'customers/alphabetized', to: 'customers#alphabetized'
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
