e23532357e3b9da6bd2a9291229710c9118b6c45
Tingting Wei <twei3@academic.rrc.ca>
2024-02-28 22:47:30 -0600

    Add a few validations to the customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,5 +4,7 @@ class Customer < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "email_address", "full_name", "id", "notes", "phone_number", "updated_at"]
   end
-  
+
+  validates :full_name, presence: true
+  validates :phone_number, presence: true
 end
