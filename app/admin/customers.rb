0a27eeb3f7d2c98208d6434dbb43b61107265bf1
Tingting Wei <twei3@academic.rrc.ca>
2024-02-28 22:06:33 -0600

    Add an image to the Customer model using ActiveStorage and configure ActiveAdmin to allow you to upload this image

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -7,7 +7,7 @@ ActiveAdmin.register Customer do
   #
   actions :index, :edit, :update, :create, :destroy
 
-  permit_params :full_name, :phone_number, :email_address, :notes
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
   #
   # or
   #
@@ -17,4 +17,51 @@ ActiveAdmin.register Customer do
   #   permitted
   # end
 
+  index do
+    selectable_column
+    id_column
+    column :full_name
+    column :phone_number
+    column :email_address
+    column "Image" do |customer|
+      if customer.image.attached?
+        image_tag customer.image.variant(resize_to_limit: [100, 100])
+      else
+        "No Image"
+      end
+    end
+    actions
+  end
+
+
+  filter :full_name
+  filter :phone_number
+  filter :email_address
+
+  form do |f|
+    f.inputs 'Customer Information' do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email_address
+      f.input :notes
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :full_name
+      row :phone_number
+      row :email_address
+      row :notes
+      row :image do |cust|
+        if cust.image.attached?
+          image_tag url_for(cust.image), width: "50%"
+        else
+          "No image attached"
+        end
+      end
+    end
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,8 @@
 class Customer < ApplicationRecord
+  has_one_attached :image
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email_address", "full_name", "id", "notes", "phone_number", "updated_at"]
+  end
+  
 end
